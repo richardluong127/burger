853a46f4addccedce1fb34b39ca510cdc2547417
richardluong127 <richardluong127@gmail.com>
2019-02-20 16:16:03 -0800

    working app

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "lol127",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if(err) {
+        console.error("error connection: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger 28 Day Aged Beef', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger with Ketchup and Mustard', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fish Fillet with Tarter Sauce Burger', FALSE);
